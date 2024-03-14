9790047d7970e2f0e6482d0af74560e6cc198357
YusukeUemura <145743053+uemurayusuke@users.noreply.github.com>
2024-03-14 14:58:12 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,11 +1,4 @@
 
-//・1から100までの数値をループで処理
-//・3で割り切れる数値の場合は、「Fizz」と表示
-//・5で割り切れる数値の場合は、「Buzz」と表示
-//・3と5の両方で割り切れる数値の場合、「FizzBuzz」と表示
-//・それ以外の数値は、その数値をそのまま表示
-
-
 
 public class FizzBuzz {
 	public static void main(String[] args) {
@@ -24,47 +17,3 @@ public class FizzBuzz {
 		}
 	}
 }
-
-
-
-
-
-
-//public class FizzBuzz {
-//	public static void main(String[] args) {
-//		for (int number = 1; number <=100; number++) {
-//			
-//			if (number%3 == 0) {
-//				System.out.println("Fizz");
-////				a%bで剰余算
-//			} else if (number%5 == 0) {
-//				System.out.println("Buzz");
-//			} else if (number%3 == 0 && number%5 == 0) {
-//				System.out.println("FizzBuzz");
-//			} else {
-//				System.out.println(number);
-//			}
-//		}
-//	}
-//}
-//上記では、3と5で割り切れる時のFizzBuzzが出力できない。
-
-
-
-
-//public class FizzBuzz {
-//	public static void main(String[] args) {
-//		for (int number = 1; number <=100; number++) {
-//			System.out.println(number);
-//		}
-//	}
-//}
-//上記で1から100まで1個ずつ出力する。
-//ここに条件式を盛り込むか
-
-
-
-
-
-
-
